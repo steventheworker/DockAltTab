39705de5fe1702a4ca25d714d982d70c82e9e306
steventheworker <steventheworker@gmail.com>
2022-10-21 19:46:52 -0700

    make settimeout global (2)

--- DockAltTab/src/globals.m
+++ DockAltTab/src/globals.m
@@ -1,5 +1,5 @@
 //
-//  globals.c    -    
+//  globals.m    -    globals.c  (error https://stackoverflow.com/questions/25999754/error-message-could-not-build-module-foundation (bryan 's answer))
 //  DockAltTab
 //
 //  Created by Steven G on 10/21/22.
@@ -8,9 +8,6 @@
 #include "globals.h"
 #import <Foundation/Foundation.h>
 
-//void GSPrintTest(void) {
-//  NSLog(@"test");
-//}
 void setTimeout(void(^cb)(void), int delay) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, NSEC_PER_MSEC * (delay)), dispatch_get_main_queue(), cb);
 }
