bc66f87eda714a7ddb1c3c39e6be76ee2b2c7c6c
steventheworker <steventheworker@gmail.com>
2023-09-22 20:17:08 -0700

    start working on windows mode

--- DockAltTab/src/prefsWindowController.m
+++ DockAltTab/src/prefsWindowController.m
@@ -11,22 +11,21 @@
 #import "DockAltTab.h"
 #import "../AppDelegate.h"
 
+NSDictionary* modeDict = @{@"MacOS": @1, @"Ubuntu": @2, @"Windows": @3};
+
 @interface prefsWindowController ()
-
 @end
-
 @implementation prefsWindowController
 - (void) awakeFromNib {
     [helperLib activateWindow: [self window]]; //activate on launch
 //    //render
-    [self modeBtn: [[self radioContainer] accessibilityChildren][0 /* index of child titled Ubuntu */]];
+    [self modeBtn: [[self radioContainer] accessibilityChildren][1 /* index of child titled Windows */]];
     for (NSButtonCell* cell in [[self radioContainer] accessibilityChildren]) [cell setFocusRingType: NSFocusRingTypeNone]; // Remove NSFocusRing (focus border/outline)
 }
 - (NSView*) radioContainer {
     return [helperLib $0: self.window.contentView : @"modeBtns"];
 }
 - (IBAction)modeBtn:(id)sender {
-    NSDictionary* modeDict = @{@"MacOS": @1, @"Ubuntu": @2, @"Windows": @3};
     NSArray* children = [[self radioContainer] accessibilityChildren];
     for (NSButtonCell* cell in children) [cell setState: [cell.title isEqual: [sender title]] ? NSControlStateValueOn : NSControlStateValueOff];
     [DockAltTab setMode: [modeDict[[sender title]] intValue]];
